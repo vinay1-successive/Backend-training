8c162cc80f8d353e082043080a8e657c8549363b
vinay1-successive <vinay.tomar@successive.tech>
2023-11-28 12:58:54 +0530

    basic changes

--- src/middleware/dynamicValidation.js
+++ src/middleware/dynamicValidation.js
@@ -2,7 +2,7 @@ import schema from "../utils/Schemas.js";
 const dynamicValidation = (req, res, next) => {
   const url = req.url.split("/")[1];
   const user = req.body;
-  const validateFun = schema.login;
+  const validateFun = schema[url];
   const { error, value } = validateFun.validate(user);
   if (error) {
     return res.status(401).send("Unauthorized");

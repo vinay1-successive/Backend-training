13e8c7818edb82525026aba6a14471f6bca80863
vinay1-successive <vinay.tomar@successive.tech>
2023-11-30 12:13:03 +0530

    Basic Changes

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,14 +1,18 @@
 import jwt from "jsonwebtoken";
 const secret = "Vinay@1234";
 const authToken = (req, res, next) => {
-  const token = req.headers["authorization"].split(" ")[1];
-  console.log(token);
   try {
+    const bearer = req.headers["authorization"].split(" ")[0];
+    if (bearer !== "bearer") {
+      throw new Error();
+    }
+    const token = req.headers["authorization"].split(" ")[1];
+    console.log(token);
     const user = jwt.verify(token, secret);
     req.user = user;
     next();
   } catch (error) {
-    return res.sendStatus(401);
+    return res.status(401).send("Unauthorized");
   }
 };
 
